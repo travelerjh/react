92e62f0b8b74e5e30d16edfb91f3036aeba82a76
Jae Hee <travel237corp@naver.com>
2023-10-31 16:19:44 +0900

    인라인스타일 쓰는법

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 function App() {
-  const num1 = Math.ceil(Math.random() * 6);
-  const num2 = Math.ceil(Math.random() * 6);
-
-  let messageClassName = "lose";
-  let message = "다시던저보세요";
-
-  if (num1 === num2) {
-    messageClassName = "win";
-    message = "당첨";
-  }
+  const mystyle = {
+    color: "blue",
+    background: "gold", // black-ground 안됨   카멜 케이스로 backGround
+    fontSize: "30px",
+    textAlign: "center",
+  }; // js object
 
   return (
     <>
-      <h1> 1번주사의 번호는 {num1}</h1>
-      <h1> 2번주사의 번호는 {num2}</h1>
-      <h1 className={messageClassName}> {message}</h1>
+      <div style={mystyle}> Lorem ipsum dolor.</div>
+      {/*  {{ }} 이렇게 쓰는 이유는 자바스크립트쓰고  인라인스타일쓰려고  */}
+      <div
+        style={{ color: "red", backgroundColor: "skyblue", fontSize: "30px" }}
+      >
+        {" "}
+        Lorem ipsum dolor.
+      </div>
     </>
   );
 }
