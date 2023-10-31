743d8ebda961afa5a0d18b075e94aa49eb95bdcc
Jae Hee <travel237corp@naver.com>
2023-10-31 12:15:32 +0900

    component , 주석 , jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 function App() {
   return (
     <div>
-
+      <MyCompoent1/>
+      <MyCompoent2/>
     </div>
   );
 }
+
+function  MyCompoent1(){
+  return  <div> Hello Component</div>
+}
+
+function  MyCompoent2(){
+  return (
+           <>
+             <p> Lorem ipsum dolor sit. </p>
+              <ul>
+                <li>lorem</li>
+                <li>ipsum</li>
+                <li>dolor</li>
+              </ul>
+           </>
+
+          )
+
+}
+
 
 export default App;
