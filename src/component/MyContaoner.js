26ac704187829e1b0f2b4bec1c74f917d1344fce
Jae Hee <travel237corp@naver.com>
2023-11-01 16:29:05 +0900

    `` 1 왼쪽에 있는 버튼 사용법

--- src/component/MyContaoner.js
+++ src/component/MyContaoner.js
@@ -2,7 +2,7 @@ import css from "../style/style2.module.css";
 
 export function MyContaoner() {
   return (
-    <div className={css.boxClass}>
+    <div className={`${css.boxClass}  ${css.error} ${css.note}`}>
       <h1>Lorem ipsum dolor sit.</h1>
     </div>
   );
